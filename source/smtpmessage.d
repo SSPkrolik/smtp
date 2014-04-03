c753b984ced71b725cfda71cf9a70856b4f9ea64
Rostyslav Dzinko <rostislav.dzinko@gmail.com>
2014-04-04 02:04:05 +0300

    Added fields and properties to SmtpMessage class

--- source/smtpmessage.d
+++ source/smtpmessage.d
@@ -7,11 +7,26 @@ import std.stdio;
 class SmtpMessage {
 
 private:
+	string m_sender;
+	string[] m_recipients;
+	string m_message;
+	string m_subject;
+	string m_replyTo;
 
 public:
-	this(string sender, string[] recipients) {
-
+	this(string sender, string[] recipients, string subject = "", string message = "", string replyTo = "") {
+		this.m_sender = sender.dup;
+		this.m_recipients = recipients.dup;
+		this.m_subject = subject.dup;
+		this.m_message = message.dup;
+		this.m_replyTo = replyTo.dup;
 	}
+
+	@property string sender() { return m_sender.dup; }
+	@property string[] recipients() { return m_recipients.dup; }
+	@property string message() { return m_message.dup; }
+	@property string subject() { return m_subject.dup; }
+	@property string replyTo() { return m_replyTo.dup; }
 
 	override string toString() {
 		return "";

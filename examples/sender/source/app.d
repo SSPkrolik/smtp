736de7074ca3385d9d963b17fc8babcdcef0dff4
Rostyslav Dzinko <rostislav.dzinko@gmail.com>
2014-04-14 22:46:38 +0300

    Added MailSender capability to find out server-supported extensions, added example of high-level API usage

--- examples/sender/source/app.d
+++ examples/sender/source/app.d
@@ -1,6 +1,47 @@
+/++
+ Example: smtp library `MailSender` high-level API usage.
+ 
+ Typical workflow is: connect() -> [authenticate()] -> send() x n -> quit()
+
+ Working with SMTP server via MailSender over non-encrypted communication channel.
+ Here we send sample letter using SMTP protocol.
+
+ ! WARNING ! You can mention strange code here, which is though made in such a special
+ manner to demonstrate as much features of the library as possible.
+ +/
 import std.stdio;
+
+import smtp.auth;
+import smtp.mailsender;
+import smtp.message;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	// High-level SMTP client - `MailSender` instance.
+	auto sender = new MailSender("localhost", 25);
+
+	// Connecting to server
+	auto result = sender.connect();
+	if (result.success) {
+		// Performing authentication using PLAIN method with login and password
+		write(sender.authenticate(SmtpAuthType.PLAIN, "login", "password"));
+
+		// Creating `SmtpMssage` - convinient struct to create valid formed SMTP message
+		auto message = SmtpMessage(
+			Recipient("from@example.com", "From"),
+			[Recipient("to@example.com", "To")],
+			"Subject",
+			"This is a message body",
+			"replyto@example.com",
+		);
+
+		// Send message
+		write(sender.send(message));
+	} else {
+		write(result);
+		return;
+	}
+	
+	// Bye-bye
+	sender.quit();
 }
--- source/smtp/mailsender.d
+++ source/smtp/mailsender.d
@@ -1,5 +1,11 @@
 module smtp.mailsender;
 
+import std.algorithm;
+import std.conv;
+import std.stdio;
+import std.string;
+import std.traits;
+
 import smtp.auth;
 import smtp.client;
 import smtp.message;
@@ -18,19 +24,103 @@ private:
 	version(ssl) {
 	EncryptionMethod _encType;
 	}
+	bool _server_supports_pipelining = false;
+	bool _server_supports_vrfy = false;
+	bool _server_supports_etrn = false;
+	bool _server_supports_enhanced_status_codes = false;
+	bool _server_supports_dsn = false;
+	bool _server_supports_8bitmime = false;
+	bool _server_supports_binarymime = false;
+	bool _server_supports_chunking = false;
+
+	uint _max_message_size = 0;
 
 public:
 version(ssl) {
+	/++
+	 SSL-enabled constructor
+	 +/
 	this(string host, ushort port, EncryptionMethod encType = EncryptionMethod.None) {
 		super(host, port);
 		_encType = encType;
 	}
 } else {
+	/++
+	 No-SSL constructor
+	 +/
 	this(string host, ushort port) {
 		super(host, port);
 	}
 }
 
+	/++
+	 Server limits
+	 +/
+	uint maxMessageSize() const { return _max_message_size; }
+
+	/++
+	 Server-supported extensions
+	 +/
+	bool extensionPipelining() const { return _server_supports_pipelining; }
+	bool extensionVrfy() const { return _server_supports_vrfy; }
+	bool extensionEtrn() const { return _server_supports_etrn; }
+	bool extensionEnhancedStatusCodes() const { return _server_supports_enhanced_status_codes; }
+	bool extensionDsn() const { return _server_supports_dsn; }
+	bool extension8bitMime() const { return _server_supports_8bitmime; }
+	bool extensionBinaryMime() const { return _server_supports_binarymime; }
+	bool extensionChunking() const { return _server_supports_chunking; }
+
+	/++
+	 Connecting to SMTP server and also trying to get server possibiities
+	 in order to expose it via public API.
+	 +/
+	override SmtpReply connect() {
+		auto reply = super.connect();
+		if (!reply.success) return reply;
+		
+		// Trying to get what possibilities server supports
+		reply = ehlo();
+		foreach(line; split(strip(reply.message), "\r\n")[1 .. $ - 1]) {
+			auto extension = line[4 .. $];
+			switch(extension) {
+			case "PIPELINING":
+				_server_supports_pipelining = true;
+				break;
+			case "VRFY":
+				_server_supports_vrfy = true;
+				break;
+			case "ETRN":
+				_server_supports_etrn = true;
+				break;
+			case "ENHANCEDSTATUSCODES":
+				_server_supports_enhanced_status_codes = true;
+				break;
+			case "DSN":
+				_server_supports_dsn = true;
+				break;
+			case "8BITMIME":
+				_server_supports_8bitmime = true;
+				break;
+			case "BINARYMIME":
+				_server_supports_binarymime = true;
+				break;
+			default:
+			}
+		}
+
+		foreach(line; split(strip(reply.message), "\r\n")[1 .. $]) {
+			auto option = line[4 .. $];
+			if (option.startsWith("SIZE")) {
+				_max_message_size = to!int(line[9 .. $]);
+				continue;
+			}
+			if (option.startsWith("CHUNKING")) {
+
+			}
+		}
+
+		return reply;
+	}
 	/++
 	 Perfrom authentication process in one method (high-level) instead
 	 of sending AUTH and auth data in several messages.
@@ -44,11 +134,11 @@ version(ssl) {
 	 SmtpReply authenticate(A...)(in SmtpAuthType authType, A params) {
 	 	final switch (authType) {
 	 	case SmtpAuthType.PLAIN:
-	 		static assert(params.length == 2 && isSomeString(params[0]) && isSomeString(params[1]));
+	 		static assert((params.length == 2) && is(A[0] == string) && is(A[1] == string));
 			auto reply = auth(authType);
 			return reply.success ? authPlain(params[0], params[1]) : reply;
 	 	case SmtpAuthType.LOGIN:
-	 		static assert(params.length == 2 && isSomeString(A[0]) && isSomeString(A[1]));
+	 		static assert((params.length == 2) && is(A[0] == string) && is(A[1] == string));
 			auto reply = auth(authType);
 			if (reply.success) {
 				reply = authLoginUsername(params[0]);
@@ -57,7 +147,6 @@ version(ssl) {
 				return reply;
 			}
 	 	}
- 		return SmtpReply(false, 0, "");
 	 }
 
 	/++
